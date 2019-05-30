6851eb09e2ca8551d9b4e6326e5993d8f87d1580
jonathanRQ99 <42821701+jonathanRQ99@users.noreply.github.com>
2019-05-30 15:33:53 -0500

    Add files via upload

--- agenda/Usuario.js
+++ agenda/Usuario.js
@@ -7,6 +7,30 @@ export default class Usuario {
     }
 
     _initTables() {
-        
+        let taller = JSON.parse(localStorage.getItem("taller"));
+        if(!taller) {
+        return;
+        }
+        taller.forEach( (Agenda, index) =>{
+            console.log(Agenda);
+            Agenda.fechaN = new Date(Agenda.fechaN);
+            this._addAgenda(new Agenda(Agenda));
+
+        });
+    }
+
+    _addEditDeleteToRow(row, Agenda){
+        let btnDelete = document.createElement("input");
+        btnDelete.type = "button";
+        btnDelete.value = 'borrar';
+        btnDelete.className = 'btn btn-danger';
+        row.cells[5].innerHTML = '';
+        row.cells[5],appendChild(btnDelete);
+        btnDelete.addEventListener('click', () => {
+            this._taller.splice(Agenda, 1);
+            row.innerHTML = "";
+            localStorage.setItem("taller", JSON.stringify(this._taller));
+            return;
+        });
     }
 }
